b9a9d27e65975aabbe947f164fc2a2cc6345cf88
Ben Greenman <benjaminlgreenman@gmail.com>
2017-06-08 17:35:01 -0400

    [rst] first real draft of language

--- model/model.rkt
+++ model/model.rkt
@@ -32,25 +32,61 @@
   redex/reduction-semantics)
 
 (module+ test
-  (require rackunit-abbrevs rackunit))
+  (require rackunit-abbrevs rackunit
+           (for-syntax racket/base racket/syntax syntax/parse)))
 
 ;; =============================================================================
 
 ;; TODO what about type annotations?
 
 (define-language RST
-  (e ::= x (λ (x) e) (unbox e) (set-box! e e) (+ e e) (e e) (box e) (if e e e) (let ((x e)) e) (letrec ((x e)) e))
+  (e ::= x v (λ (x) e) (unbox e) (set-box! e e) (+ e e) (e e) (box e) (if e e e) (let ((L x e)) e) (letrec ((L x e)) e) (:: e τ))
   (v ::= c integer (box v))
-  (c ::= (CLOSURE (λ (x) e) γ))
-  (τ ::= (U k τ) (μ (α) τ) α)
+  (c ::= (CLOSURE e γ))
+  (σ ::= (∀ (α) σ) τ)
+  (τ ::= (U k τ) (μ (α) τ) α k)
   (k ::= Integer (→ τ τ) (Box τ))
   (L ::= R S T)
-  (P ::= (R e))
   (γ ::= ((L x v) ...))
   (Γ ::= ((L x τ) ...))
   (x α ::= variable-not-otherwise-mentioned)
-  ;#:binding-forms ???
+  #:binding-forms
+  (λ (x) e #:refers-to x)
+  (let ((x e_0)) e #:refers-to x)
+  (letrec ((x e_0 #:refers-to x)) e #:refers-to x)
 )
+
+(module+ test
+  (define-syntax (define-predicate* stx)
+    (syntax-parse stx
+     [(_ [x*:id ...])
+      #:with (x?* ...) (for/list ([x (in-list (syntax-e #'(x* ...)))]) (format-id stx "~a?" (syntax-e x)))
+      (syntax/loc stx (begin (define x?* (redex-match? RST x*)) ...))]))
+
+  (define-predicate* [e v c σ τ k L γ Γ])
+
+  (test-case "e"
+    (check-pred e? (term x))
+    (check-pred e? (term 4))
+    (check-pred e? (term (:: 4 Integer)))
+    (check-pred e? (term (:: 4 (Box Integer))))
+    (check-pred e? (term (let ((R x 4)) (+ x 1)))))
+
+  (test-case "τ"
+    (check-pred τ? (term Integer)))
+)
+
+;; -----------------------------------------------------------------------------
+
+;; -----------------------------------------------------------------------------
+
+
+;; -----------------------------------------------------------------------------
+
+;; -----------------------------------------------------------------------------
+
+
+;; -----------------------------------------------------------------------------
 
 ;; The colored interpreter needs to keep types at runtime. For what?
 ;; - ???
