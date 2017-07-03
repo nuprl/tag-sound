69377c49551e1e13fea93a729fe5b91221c344ee
Ben Greenman <benjaminlgreenman@gmail.com>
2017-07-03 18:06:40 -0400

    [simple-rst] checkpoint, write language start type system

--- model/stlc.rkt
+++ model/stlc.rkt
@@ -8,11 +8,17 @@
 ;; - mon, why/how not nested???
 ;; - lemma `∀L . ⊢L mon(t,e,L+) : t`
 
+;; - (check-type (T e) τ)
+;;   implies (check-type (S e) τ)
+;;   implies (check-type (R e) τ)
+;; - 
+
 ;; =============================================================================
 
 (require
   racket/set
-  redex/reduction-semantics)
+  redex/reduction-semantics
+  redex-abbrevs)
 
 (define *debug* (make-parameter #f))
 
@@ -27,6 +33,244 @@
 
 ;; =============================================================================
 
+;; Simple [Racket Shallow Typed] language
+(define-language RST
+  (e ::= v (e e) x (let (x τ P) e) (+ e e) (= e e) (if e e e) (and e e) (mon L τ P) (pre-mon L τ P))
+  (v ::= integer boolean (λ (x τ) e) (mon L τ (L v)))
+  (P ::= (L e))
+  (τ ::= Int Bool (Pair τ τ) (→ τ τ) TST)
+  (L ::= R S T)
+  (L\R ::= S T)
+  (Γ ::= ((x τ) ...))
+  (E ::= hole
+         (E e) (v E)
+         (+ E e) (+ v E)
+         (= E e) (= v E)
+         (if E e e)
+         (and E e))
+  (x ::= (variable-not-otherwise-mentioned))
+#:binding-forms
+  (λ (x τ) e #:refers-to x))
+
+;; -----------------------------------------------------------------------------
+;; --- check-type
+
+(define-judgment-form RST
+  #:mode (check-type I I I)
+  #:contract (check-type Γ P τ)
+  [
+   (infer-type Γ (R e) TST)
+   ---
+   (check-type Γ (R e) τ)]
+  [
+   (infer-type Γ (S e) #{tag-only τ})
+   ---
+   (check-type Γ (S e) τ)]
+  [
+   (infer-type Γ (T e) τ)
+   ---
+   (check-type Γ (T e) τ)])
+
+(define-judgment-form RST
+  #:mode (infer-type I I O)
+  #:contract (infer-type Γ P TST)
+  [
+   ---
+   (infer-type Γ (R integer) TST)]
+  [
+   ---
+   (infer-type Γ (L\R integer) Int)]
+  [
+   ---
+   (infer-type Γ (R boolean) TST)]
+  [
+   ---
+   (infer-type Γ (L\R boolean) TST)]
+;  [
+;   (where Γ_x #{type-env-set Γ (x τ)})
+;   (infer-type-R Γ_x e TST)
+;   ---
+;   (infer-type-R Γ (λ (x τ) e) TST)]
+;  [
+;   (infer-type-R Γ e_0 TST)
+;   (infer-type-R Γ e_1 TST)
+;   ---
+;   (infer-type-R Γ (e_0 e_1) TST)]
+;  [
+;   (where τ #{type-env-ref Γ x})
+;   ---
+;   (infer-type-R Γ x TST)]
+;  [
+;   (check-type Γ P τ)
+;   (where Γ_x #{type-env-set Γ (x τ)})
+;   (infer-type-R Γ_x e TST)
+;   ---
+;   (infer-type-R Γ (let (x τ P) e) TST)]
+;  [
+;   (infer-type-R Γ e_0 TST)
+;   (infer-type-R Γ e_1 TST)
+;   ---
+;   (infer-type-R Γ (+ e_0 e_1) TST)]
+;  [
+;   (infer-type-R Γ e_0 TST)
+;   (infer-type-R Γ e_1 TST)
+;   ---
+;   (infer-type-R Γ (= e_0 e_1) TST)]
+;  [
+;   (infer-type-R Γ e_0 TST)
+;   (infer-type-R Γ e_1 TST)
+;   (infer-type-R Γ e_2 TST)
+;   ---
+;   (infer-type-R Γ (if e_0 e_1 e_2) TST)]
+;  [
+;   (infer-type-R Γ e_0 TST)
+;   (infer-type-R Γ e_1 TST)
+;   ---
+;   (infer-type-R Γ (and e_0 e_1) TST)]
+;  [
+;   (check-type P τ) ;; maybe too strict
+;   ---
+;   (infer-type-R Γ (mon R τ P) TST)]
+;  [
+;   (check-type P τ)
+;   ---
+;   (infer-type-R Γ (pre-mon R τ P) TST)]
+)
+
+;;(define-judgment-form RST
+;;  #:mode (infer-type-S I I I)
+;;  #:contract (infer-type-S Γ e τ)
+;;  [
+;;   ---
+;;   (infer-type-S Γ integer Int)]
+;;  [
+;;   ---
+;;   (infer-type-S Γ boolean Bool)]
+;;  [
+;;   (where Γ_x #{type-env-set Γ (x τ_dom)})
+;;   (infer-type-S Γ_x e τ_cod)
+;;   ---
+;;   (infer-type-S Γ (λ (x τ_dom) e) (→ τ_dom τ_cod))]
+;;  [
+;;   (infer-type-S Γ e_0 (→ τ_dom τ_cod))
+;;   (infer-type-S Γ e_1 τ_1)
+;;   (type= τ_dom τ_1)
+;;   ---
+;;   (infer-type-S Γ (e_0 e_1) τ_cod)]
+;;  [
+;;   (where τ #{type-env-ref Γ x})
+;;   ---
+;;   (infer-type-S Γ x τ)]
+;;  [
+;;   (check-type P τ)
+;;   (where Γ_x #{type-env-set Γ (x τ)})
+;;   (infer-type-S Γ_x e τ_e)
+;;   ---
+;;   (infer-type-S Γ (let (x τ P) e) τ_e)]
+;;  [
+;;   (infer-type-S Γ e_0 Int)
+;;   (infer-type-S Γ e_1 Int)
+;;   ---
+;;   (infer-type-S Γ (+ e_0 e_1) Int)]
+;;  [
+;;   (infer-type-S Γ e_0 Int)
+;;   (infer-type-S Γ e_1 Int)
+;;   ---
+;;   (infer-type-S Γ (= e_0 e_1) Bool)]
+;;  [
+;;   (infer-type-S Γ e_0 τ_0)
+;;   (infer-type-S Γ e_1 τ_1)
+;;   (infer-type-S Γ e_2 τ_2)
+;;   (type= τ_1 τ_2)
+;;   ---
+;;   (infer-type-S Γ (if e_0 e_1 e_2) τ_2)]
+;;  [
+;;   (infer-type-S Γ e_0 Bool)
+;;   (infer-type-S Γ e_1 Bool)
+;;   ---
+;;   (infer-type-S Γ (and e_0 e_1) Bool)]
+;;  [
+;;   (check-type P τ) ;; maybe too strict
+;;   ---
+;;   (infer-type-S Γ (mon S τ P) τ)]
+;;  [
+;;   (check-type P τ)
+;;   ---
+;;   (infer-type-S Γ (pre-mon S τ P) τ)]
+;;)
+;;
+;;(define-judgment-form RST
+;;  #:mode (check-type-T I I I)
+;;  #:contract (check-type-T Γ e τ)
+;;)
+
+;; -----------------------------------------------------------------------------
+;; --- infer-type
+
+;; -----------------------------------------------------------------------------
+;; --- type helpers
+
+(define-metafunction RST
+  tag-only : τ -> τ
+  [(tag-only Int)
+   Int]
+  [(tag-only Bool)
+   Bool]
+  [(tag-only (Pair τ_0 τ_1))
+   (Pair TST TST)]
+  [(tag-only (→ τ_dom τ_cod))
+   (→ TST TST)]
+  [(tag-only TST)
+   TST])
+
+(define-metafunction RST
+  coerce-→ : τ -> τ
+  [(coerce-→ TST)
+   (→ TST TST)]
+  [(coerce-→ (→ τ_dom τ_cod))
+   (→ τ_dom τ_cod)]
+  [(coerce-→ τ)
+   ,(raise-argument-error 'coerce-→ "cannot coerce ~a" (term τ))])
+
+(define-judgment-form RST
+  #:mode (type= I I)
+  #:contract (type= τ τ)
+  [
+   --- Refl
+   (type= τ τ)])
+
+(module+ test
+  (test-case "tag-only"
+    (check-mf-apply*
+     ((tag-only Int)
+      Int)
+     ((tag-only (→ Int Bool))
+      (→ TST TST))
+     ((tag-only (Pair (→ Int Int) Bool))
+      (Pair TST TST))))
+
+  (test-case "coerce-→"
+    (check-mf-apply*
+     ((coerce-→ (→ Int Bool))
+      (→ Int Bool))
+     ((coerce-→ TST)
+      (→ TST TST))))
+)
+
+;; -----------------------------------------------------------------------------
+;; --- environment helpers
+
+(define-metafunction RST
+  type-env-set : Γ (x τ) -> Γ
+  [(type-env-set Γ (x τ))
+   ,(cons (term (x τ)) (term Γ))])
+
+(define-metafunction RST
+  type-env-ref : Γ x -> any
+  [(type-env-ref Γ x)
+   ,(for/first ([xτ (in-list (term Γ))]
+                #:when (eq? (term x) (car xτ)))
+      (cadr xτ))])
 
 ;; -----------------------------------------------------------------------------
 ;; --- evalution
