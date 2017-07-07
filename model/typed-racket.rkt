dd972ed509c81abbea35ec19aa83064e5184fbfd
Ben Greenman <benjaminlgreenman@gmail.com>
2017-07-06 22:14:23 -0400

    typed-racket: evaluation, old tests pass, idgaf

--- model/typed-racket.rkt
+++ model/typed-racket.rkt
@@ -10,8 +10,12 @@
 ;;   - `e` raises a boundary error `b` that points to a _specific location_
 ;;     where an untyped value entered typed code.
 
+;; TODO
+;; - label boundaries
+
 ;; Lemmas
 ;;   - `∀ e . ⊢T e : τ => τ != TST`
+;;   - `∀ (mon L_ctx τ_ctx (L_v v)) . ⊢L_v v : τ_ctx`
 
 ;; -----------------------------------------------------------------------------
 
@@ -113,6 +117,16 @@
    ---
    (infer-type Γ (T boolean) Bool)]
   [
+   (infer-type Γ (R e_0) τ_0)
+   (infer-type Γ (R e_1) τ_1)
+   ---
+   (infer-type Γ (R (cons e_0 e_1)) TST)]
+  [
+   (infer-type Γ (T e_0) τ_0)
+   (infer-type Γ (T e_1) τ_1)
+   ---
+   (infer-type Γ (T (cons e_0 e_1)) (Pair τ_0 τ_1))]
+  [
    (where Γ_x #{type-env-set Γ (x τ_dom)})
    (infer-type Γ_x (R e) τ_cod)
    ---
@@ -186,12 +200,11 @@
    (infer-type Γ (R (if e_0 e_1 e_2)) TST)]
   [
    (infer-type Γ (T e_0) τ_0)
-   (type= τ_0 Bool)
    (infer-type Γ (T e_1) τ_1)
    (infer-type Γ (T e_2) τ_2)
-   (type= τ_1 τ_2)
+   (where τ_3 #{make-union τ_1 τ_2})
    ---
-   (infer-type Γ (T (if e_0 e_1 e_2)) τ_2)]
+   (infer-type Γ (T (if e_0 e_1 e_2)) τ_3)]
   [
    (infer-type Γ (R e_0) τ_0)
    (infer-type Γ (R e_1) τ_1)
@@ -200,10 +213,9 @@
   [
    (infer-type Γ (T e_0) τ_0)
    (infer-type Γ (T e_1) τ_1)
-   (type= τ_0 Bool)
-   (type= τ_1 Bool)
+   (where τ_2 #{make-union τ_0 τ_1})
    ---
-   (infer-type Γ (T (and e_0 e_1)) Bool)]
+   (infer-type Γ (T (and e_0 e_1)) τ_2)]
   [
    (check-type Γ P τ)
    ---
@@ -260,12 +272,262 @@
      (well-typed (R ((mon R (→ Int Int) (T (λ (x Int) 2))) 7)))
      (well-typed (T (let (x Int (T 1)) (let (y Int (T 2)) (+ x y)))))
      (check-type () (T 1) Int)
+     (check-type () (T 1) (U Int (Pair Int Int)))
+     (well-typed (T (if 1 2 (cons 2 3))))
+     (check-type () (T (and 1 #true)) (U Bool Int))
+     (well-typed (T (if (λ (x Int) x) 1 0)))
      (well-typed (T (let (x Int (T 1)) (let (y Int (T 2)) (+ x y)))))
     )
-
-    (check-false (judgment-holds (well-typed (T (if (λ (x Int) x) 1 0)))))
   )
 )
+
+;; -----------------------------------------------------------------------------
+;; --- evalution
+
+(define -->μTR
+  (reduction-relation μTR
+    #:domain A
+;; -- MON
+    [-->
+     (L (in-hole E (pre-mon L_ctx τ_ctx P)))
+     (L (in-hole E (pre-mon L_ctx τ_ctx P_step)))
+     PreMon-Step
+     (where (P_step) ,(apply-reduction-relation -->μTR (term P)))]
+    [-->
+     (L (in-hole E (pre-mon L_ctx τ_ctx P)))
+     RuntimeError
+     PreMon-Error
+     (where (RuntimeError) ,(apply-reduction-relation -->μTR (term P)))]
+    [-->
+     (R (in-hole E (pre-mon R τ_ctx (T v))))
+     (R (in-hole E v_+))
+     PreMon-T->R
+     (where v_+ ,(if (judgment-holds (flat T τ_ctx))
+                   (term v)
+                   (term (mon L_ctx τ_ctx (L_v v)))))]
+    [-->
+     (L (in-hole E (pre-mon L τ (L v))))
+     (L (in-hole E v))
+     PreMon-NoBoundary]
+    [-->
+     (T (in-hole E (pre-mon T τ_ctx (R v))))
+     (T (in-hole E v_+))
+     PreMon-FinerContext-MaybeOk
+     (judgment-holds (dynamic-typecheck (T v) τ_ctx))
+     (where v_+ ,(if (judgment-holds (flat T τ_ctx))
+                   (term v)
+                   (term (mon T τ_ctx (R v)))))]
+    [-->
+     (T (in-hole E (pre-mon T τ_ctx (R v))))
+     (BoundaryError T τ_ctx (R v))
+     PreMon-FinerContext-NotOk
+     (side-condition (not (judgment-holds (dynamic-typecheck (T v) τ_ctx))))]
+;; -- APP
+    [-->
+     (L (in-hole E ((λ (x τ) e) v_1)))
+     (L (in-hole E (substitute e x v_1)))
+     App-Beta]
+    [-->
+     (L (in-hole E ((mon L_ctx (→ τ_dom τ_cod) (L_λ v)) v_1)))
+     (L (in-hole E (pre-mon L_ctx τ_cod (L_λ (v (pre-mon L_λ τ_dom (L_ctx v_1)))))))
+     App-Mon]
+    [-->
+     (R (in-hole E (v_0 v_1)))
+     (DynError (R (v_0 v_1)))
+     App-Error
+     (side-condition (not (judgment-holds (proc? v_0))))]
+;; -- LET
+    [-->
+     (L (in-hole E (let (x τ P) e_body)))
+     (L (in-hole E (let (x τ P_step) e_body)))
+     Let
+     (where (P_step) ,(apply-reduction-relation -->μTR (term P)))]
+    [-->
+     (L (in-hole E (let (x τ (L_v v)) e_body)))
+     (L (in-hole E ((λ (x τ) e_body) (pre-mon L τ (L_v v)))))
+     Let-Beta]
+;; -- Primop, If, etc
+    [-->
+     (L (in-hole E (+ integer_0 integer_1)))
+     (L (in-hole E ,(+ (term integer_0) (term integer_1))))
+     +]
+    [-->
+     (R (in-hole E (+ v_0 v_1)))
+     (DynError (R (+ v_0 v_1)))
+     +-Error
+     (side-condition (or (not (integer? (term v_0))) (not (integer? (term v_1)))))]
+    [-->
+     (L (in-hole E (= integer_0 integer_1)))
+     (L (in-hole E ,(= (term integer_0) (term integer_1))))
+     =]
+    [-->
+     (R (in-hole E (= v_0 v_1)))
+     (DynError (R (= v_0 v_1)))
+     =-Error
+     (side-condition (or (not (integer? (term v_0))) (not (integer? (term v_1)))))]
+    [-->
+     (L (in-hole E (and boolean_0 boolean_1)))
+     (L (in-hole E ,(and (term boolean_0) (term boolean_1))))
+     And]
+    [-->
+     (R (in-hole E (and v_0 v_1)))
+     (DynError (R (and v_0 v_1)))
+     And-Error
+     (side-condition (or (not (boolean? (term v_0))) (not (boolean? (term v_1)))))]
+    [-->
+     (L (in-hole E (if v e_1 e_2)))
+     (L (in-hole E e_1))
+     If-True
+     (side-condition (not (eq? #false (term v))))]
+    [-->
+     (L (in-hole E (if #false e_1 e_2)))
+     (L (in-hole E e_2))
+     If-False]))
+
+(define -->μTR*
+  (make--->* -->μTR))
+
+(define-metafunction μTR
+  eval : P -> A
+  [(eval P)
+   A
+   (judgment-holds (well-typed P))
+   (where A ,(-->μTR* (term P)))]
+  [(eval P)
+   ,(raise-user-error 'eval "trouble eval'ing ~a" (term P))
+   (judgment-holds (well-typed P))]
+  [(eval P)
+   ,(raise-user-error 'eval "typechecking failed ~a" (term P))])
+
+(module+ test
+  (test-case "eval:R:I"
+    ;; simplest terms, R language
+    (check-mf-apply*
+     ((eval (R (if 1 2 3)))
+      (R 2))
+     ((eval (R (if #false 2 3)))
+      (R 3))
+     ((eval (R (if #true 2 3)))
+      (R 2))
+     ((eval (R (and 1 2)))
+      (DynError (R (and 1 2))))
+     ((eval (R (and #true 3)))
+      (DynError (R (and #true 3))))
+     ((eval (R (and #true #false)))
+      (R #false))
+     ((eval (R (and #false #true)))
+      (R #false))
+     ((eval (R (and #true #true)))
+      (R #true))
+     ((eval (R (= 1 1)))
+      (R #true))
+     ((eval (R (= 1 2)))
+      (R #false))
+     ((eval (R (= #true 2)))
+      (DynError (R (= #true 2))))
+     ((eval (R (= 3 (= 1 1))))
+      (DynError (R (= 3 #true))))
+     ((eval (R (+ 2 2)))
+      (R 4))
+     ((eval (R (+ #true 2)))
+      (DynError (R (+ #true 2))))
+     ((eval (R (+ 2 #true)))
+      (DynError (R (+ 2 #true))))
+    )
+  )
+
+  (test-case "eval:R:II"
+    (check-mf-apply*
+      ((eval (R (let (n1 Bool (R #false)) n1)))
+       (R #false))
+      ((eval (R (let (n1 Int (R (+ 2 2))) (+ n1 n1))))
+       (R 8))
+      ((eval (R ((λ (x TST) (+ x 1)) 1)))
+       (R 2))
+      ((eval (R (1 1)))
+       (DynError (R (1 1))))
+      ((eval (R ((mon R (→ Int Int) (T (λ (x Int) 2))) 7)))
+       (R 2))
+    )
+  )
+
+  (test-case "eval:simple:T"
+    (check-mf-apply*
+     [(eval (T 4))
+      (T 4)]
+     ((eval (T (if (and #true (= 4 2)) (+ 1 1) (+ 2 2))))
+      (T 4))
+     [(eval (T #true))
+      (T #true)]
+     [(eval (T (+ 2 2)))
+      (T 4)]
+     [(eval (T ((λ (x Int) x) 1)))
+      (T 1)]
+     [(eval (T ((λ (x Int) (+ x 1)) 1)))
+      (T 2)]
+     [(eval (T (if ((λ (x Bool) x) #true) 1 0)))
+      (T 1)]
+     [(eval (T (if #false (+ 6 6) 0)))
+      (T 0)]
+     [(eval (T (let (x Int (T 1))
+                 (let (y Int (T 2))
+                   (+ x y)))))
+      (T 3)]
+     [(eval (T (let (negate (→ Bool Bool) (T (λ (x Bool) (if x #false #true))))
+                 (let (b Bool (T #false))
+                   (negate (negate b))))))
+      (T #false)]
+     [(eval (T (let (x Int (T 4))
+                 (let (add-x (→ Int Int) (T (λ (y Int) (+ y x))))
+                   (let (x Int (T 5))
+                     (add-x x))))))
+      (T 9)]
+    )
+  )
+
+  (test-case "eval:simple:T:fail"
+    (check-exn #rx"typechecking failed"
+      (λ () (term #{eval (T ((λ (x Int) (+ x 1)) #false))})))
+
+    (check-exn #rx"typechecking failed"
+      (λ () (term (eval (T (let (x Bool (T (+ 2 5))) x)))))))
+
+  (test-case "apply-R-in-T"
+    (check-mf-apply*
+     [(eval (T (let (f (→ Int Int) (R (λ (x TST) (+ x 1))))
+                 (f 3))))
+      (T 4)]
+     [(eval (T (let (f (→ Int Bool) (R (λ (x TST) (if (= 0 x) #false #true))))
+                 (f 3))))
+      (T #true)]
+     [(eval (T (let (f (→ Int Int) (R (λ (x TST) #false))) (f 1))))
+      (BoundaryError T Int (R #false))]
+     [(eval (T (let (f (→ Int Int) (R (λ (x TST) (+ x #false)))) (f 3))))
+      (DynError (R (+ 3 #false)))]
+     [(eval (T (let (f (→ Int Int) (R (λ (x TST) (+ #true #false)))) (f 3))))
+      (DynError (R (+ #true #false)))]
+    )
+  )
+
+  #;(test-case "double-wrap"
+    ;; TODO no exception raised!
+    (check-exn #rx"T expected \\(→ Bool Bool\\) fuck"
+      (λ () (term
+        (eval (T (let (h (→ Bool Bool)
+                         (R (let (g (→ Int Int)
+                                    (T (let (f (→ Int Int)
+                                               (R (λ (x TST) x)))
+                                         f)))
+                              g)))
+                   (h #true))))))))
+
+
+)
+
+
+;; =============================================================================
+;; === less interesting from here on
+;; =============================================================================
 
 ;; -----------------------------------------------------------------------------
 ;; --- type helpers
@@ -350,6 +612,19 @@
    ---
    (subtype (U τ_lhs τ_0 ...) (U τ_1 ... τ_rhs τ_2 ...))])
 
+(define-metafunction μTR
+  make-union : τ τ -> τ
+  [(make-union TST τ)
+   ,(raise-argument-error 'make-union "cannot make union with TST" 0 (term TST) (term τ))]
+  [(make-union τ TST)
+   ,(raise-argument-error 'make-union "cannot make union with TST" 1 (term τ) (term TST))]
+  [(make-union τk_0 τ)
+   (make-union (U τk_0) τ)]
+  [(make-union τ τk_1)
+   (make-union τ (U τk_1))]
+  [(make-union (U τ_0 ...) (U τ_1 ...))
+   #{type-normalize (U τ_0 ... τ_1 ...)}])
+
 (module+ test
   (test-case "coerce-→"
     (check-mf-apply*
@@ -395,4 +670,87 @@
    ,(for/first ([xτ (in-list (term Γ))]
                 #:when (eq? (term x) (car xτ)))
       (cadr xτ))])
+
+;; -----------------------------------------------------------------------------
+;; --- flat types
+
+;; `(flat L τ)` iff `τ` is strictly positive and `L` fully-checks values with type `τ`
+(define-judgment-form μTR
+  #:mode (flat I I)
+  #:contract (flat L τ)
+  [
+   ---
+   (flat T Int)]
+  [
+   ---
+   (flat T Bool)]
+  [
+   (flat T τ_0)
+   (flat T τ_1)
+   ---
+   (flat T (Pair τ_0 τ_1))])
+
+(module+ test
+  (test-case "flat"
+    (check-true (judgment-holds (flat T Int)))
+    (check-false (judgment-holds (flat R Int)))
+    (check-false (judgment-holds (flat T (→ Bool Bool))))
+
+    (check-true (judgment-holds (flat T (Pair Int Int))))
+    (check-false (judgment-holds (flat T (Pair Int (Pair (→ Int Int) Int)))))
+  )
+)
+
+;; -----------------------------------------------------------------------------
+;; --- dynamic-typecheck
+
+;; Only called when τ is from "finer" language than P
+(define-judgment-form μTR
+  #:mode (dynamic-typecheck I I)
+  #:contract (dynamic-typecheck (L v) τ)
+  [
+   (side-condition ,(raise-user-error 'dynamic-typecheck "language R has no dynamic typechecker ~a ~a" (term e) (term τ)))
+   --- R
+   (dynamic-typecheck (R v) τ)]
+  [
+   ---
+   (dynamic-typecheck (T integer) Int)]
+  [
+   ---
+   (dynamic-typecheck (T boolean) Bool)]
+  [
+   (dynamic-typecheck (T v_0) τ_0)
+   (dynamic-typecheck (T v_1) τ_1)
+   ---
+   (dynamic-typecheck (T (cons v_0 v_1)) (Pair τ_0 τ_1))]
+  [
+   ---
+   (dynamic-typecheck (T (λ (x _) e)) (→ τ_dom τ_cod))]
+  [
+   ;; OK because same type
+   ---
+   (dynamic-typecheck (T (mon L_0 τ P)) τ)])
+
+(define-judgment-form μTR
+  #:mode (proc? I)
+  #:contract (proc? v)
+  [
+   ---
+   (proc? (λ (x τ) e))]
+  [
+   ;; TODO should only check type? Depends on L?
+   (proc? v_1)
+   ---
+   (proc? (mon L_0 τ_0 (L_1 v_1)))])
+
+
+(module+ test
+  (test-case "dynamic-typecheck"
+    (check-true (judgment-holds (dynamic-typecheck (T 4) Int)))
+    (check-false (judgment-holds (dynamic-typecheck (T 4) Bool)))
+
+    (check-true (judgment-holds (dynamic-typecheck (T (λ (x Int) 3)) (→ Int Int))))
+    (check-true (judgment-holds (dynamic-typecheck (T (λ (x Bool) #false)) (→ Bool Bool))))
+  )
+)
 
