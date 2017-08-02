ce4cee1a756e9b13e90506a65b82d1191e617060
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-01 23:13:43 -0400

    mts: outline judgments

--- model/machine-tag-sound.rkt
+++ model/machine-tag-sound.rkt
@@ -38,18 +38,16 @@
 
 ;; =============================================================================
 
-;; YOU KNOW WHAT, STOP IT. Forget the IR for now.
-
 (define-language++ TAG #:alpha-equivalent? α=?
   ;; --- e = source language
-  (e ::= ve x (e e) (if e e e) (let (x ?τ e) e)
+  (e ::= ve x (e e) (if e e e) (let (x τ φ e) e)
          (δ e ...))
-  (ve ::= integer boolean (fun f (x : ?τ) e))
+  (ve ::= integer boolean (fun f (x : τ) e))
   ;; eτ = intermediate language
-  ;;;; (eτ ::= vτ (x : ?τφ) ((eτ eτ) : ?τφ) ((if eτ eτ eτ) : ?τφ)
-  ;;;;         ((let (x ?τ eτ) eτ) : ?τφ)
-  ;;;;         ((binop eτ eτ) : ?τφ) ((unop eτ) : ?τφ))
-  ;;;; (vτ ::= (integer : ?τφ) (boolean : ?τφ) ((fun f (x : ?τφ) eτ) : ?τφ))
+  ;;;; (eτ ::= vτ (x : τφ) ((eτ eτ) : τφ) ((if eτ eτ eτ) : τφ)
+  ;;;;         ((let (x τ eτ) eτ) : τφ)
+  ;;;;         ((binop eτ eτ) : τφ) ((unop eτ) : τφ))
+  ;;;; (vτ ::= (integer : τφ) (boolean : τφ) ((fun f (x : τφ) eτ) : τφ))
   ;; --- c = target language
   (c ::= ;; completions
          v x (c c) (if c c c) (let (x c) c)
@@ -60,19 +58,123 @@
   (δ+ ::= make-box)
   (binop ::= + / and or set-box!)
   ;; ---
-  (?τ ::= Dyn τ)
-  ;;;; (?τφ ::= Dyn ....)
   (τ ::= (U τ0 ...) τ0)
-  (τ0 ::= (Natural φ) (Integer φ) (Boolean φ) (→ φ τ0 τ0))
-  (φ ::= + - x)
-  (κ ::= int bool proc)
-  (Γ ::= ((x τ) ...))
+  (τ0 ::= Natural Integer Boolean (Box τ) (→ τ τ))
+  (τφ ::= (U φ τφ0 ...) τφ0)
+  (τφ0 ::= (Natural φ) (Integer φ) (Boolean φ) (Box φ τφ) (→ φ τφ τφ))
+  (φ ::= ++ -- x)
+  (κ ::= int bool proc box)
+  (Γ ::= ((x τφ) ...))
   (RuntimeError ::= DivisonByZero)
   (CheckError ::= (CheckError v κ))
+  (A ::= e RuntimeError CheckError)
   (x ::= variable-not-otherwise-mentioned)
 #:binding-forms
-  (fun f (x : ?τ) e #:refers-to (shadow f x))
-  (let (x ?τ e_x) e #:refers-to x)
+  (fun f (x : τ) e #:refers-to (shadow f x))
+  (let (x τ φ e_x) e #:refers-to x)
   (fun f (x) c #:refers-to (shadow f x))
   (let (x c_x) c #:refers-to x)
 )
+
+;; -----------------------------------------------------------------------------
+
+(define-judgment-form TAG
+  #:mode (check-type I I I O)
+  #:contract (check-type Γ e τ c)
+  [
+   (infer-type Γ e c τφ_c)
+   (flag++ τφ_c)
+   (erase-φ τφ_c τ_c)
+   (type=? τ τ_c)
+   ---
+   (check-type Γ e τ c)]
+  [
+   (infer-type Γ e c τφ_c)
+   (flag-- τφ_c)
+   (erase-φ τφ_c τ_c)
+   (type=? τ τ_c)
+   (tag-of τ_c κ)
+   (where c_+ (check κ c))
+   ---
+   (check-type Γ e τ c_+)])
+
+(define-judgment-form TAG
+  #:mode (infer-type I I O O)
+  #:contract (infer-type Γ e c τφ)
+  [
+   ---
+   (infer-type Γ e 0 (Integer ++))]
+)
+
+(define-judgment-form TAG
+  #:mode (tag-of I O)
+  #:contract (tag-of τ κ)
+  [
+   ---
+   (tag-of τ int)]
+)
+
+(define -->TAG
+  (reduction-relation TAG
+    #:domain A
+    [-->
+      e
+      42]))
+
+(define -->*
+  (make--->* -->TAG))
+
+;; "term precision" ⊑
+(define-judgment-form TAG
+  #:mode (type-ann<=? I I)
+  #:contract (type-ann<=? e e)
+  [
+   ---
+   (type-ann<=? e_0 e_1)]
+)
+
+;; "more checks"
+;; This is TOO SIMPLE ... really need a simulation & compare traces
+(define-judgment-form TAG
+  #:mode (checks<=? I I)
+  #:contract (checks<=? c c)
+  [
+   ---
+   (checks<=? c_0 c_1)]
+)
+
+;; -----------------------------------------------------------------------------
+
+;; -----------------------------------------------------------------------------
+
+(define-judgment-form TAG
+  #:mode (flag++ I)
+  #:contract (flag++ τφ)
+  [
+   ---
+   (flag++ τφ)]
+)
+
+(define-judgment-form TAG
+  #:mode (flag-- I)
+  #:contract (flag-- τφ)
+  [
+   ---
+   (flag-- τφ)]
+)
+
+(define-judgment-form TAG
+  #:mode (erase-φ I O)
+  #:contract (erase-φ τφ τ)
+  [
+   ---
+   (erase-φ τφ Integer)]
+)
+
+(define-judgment-form TAG
+  #:mode (type=? I I)
+  #:contract (type=? τ τ)
+  [
+   ---
+   (type=? τ τ)])
+
