8858d4c81ac5aade76656923cfe18f475ab4ac91
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-28 17:39:39 -0400

    modal: testing o,v |= t
    - value relation, relates static type to runtime value
    - the o (sigma) is state, used to resolve boxes in v
    - proceeds by mode of t,
      - if eager mode, check the value as far as possible
      - if eager-monitor mode, higher-order values need a certificate (need a mon)
      - if lazy, accept anything but deeply-check if tag matches
        so `(cons 1 1) |= (Pair Lazy (Bool Eager) (Bool Eager))` fails
         because the contents don't match the expectation
    
    okay need to think more about the last case

--- model/modal.rkt
+++ model/modal.rkt
@@ -16,12 +16,15 @@
 ;; - corollary : soundness for TR retic TS SS
 ;; - complete monitor
 ;;
+;; - ◊ (□ ∎) are logical duals in FRP (says Tony),
+;;   are they logical duals here?
+;;  (if not, maybe ◊ should be "next" as in "after 1 check" Ο)
 ;; - union types, recursive types, forall types
 
 ;; NOTATION
 ;; □ = "A" (currently necessary)
 ;; ∎ = "W" (always necessary)
-;; ◇ = "V" (possible)
+;; ◊ = "V" (possible)
 
 ;; -----------------------------------------------------------------------------
 
@@ -44,6 +47,14 @@
 
 ;; -----------------------------------------------------------------------------
 
+;; Language MTT, key points:
+;; - recursive functions
+;; - mutable data (box)
+;; - immutable data (cons)
+;; - conditional
+;; - base types / values
+;; - modal types
+
 (define-language++ MMT #:alpha-equivalent? α=?
   (e ::= v x (e e) (if e e e) (and e e)
          (let (x τ P) e)
@@ -55,7 +66,7 @@
          (mon τ e)
          (check τ e))
   (v ::= (box x) integer boolean Λ (cons v v) (mon τ v))
-  (Λ ::= (fun f τ x e))
+  (Λ ::= (fun x τ (x) e))
   (τ ::= (k μ τ ...))
   (k ::= Int Bool Pair → Box)
   (μ ::= □ ∎ ◊)
@@ -77,7 +88,7 @@
   (x ::= variable-not-otherwise-mentioned)
 #:binding-forms
   (let (x τ P) e #:refers-to x)
-  (fun f τ x e #:refers-to (shadow f x)))
+  (fun x_f τ (x) e #:refers-to (shadow x_f x)))
 
 ;; -----------------------------------------------------------------------------
 ;; --- static type checking
@@ -88,58 +99,64 @@
 ;; -----------------------------------------------------------------------------
 ;; --- models
 
+;; v ⊨ τ
 ;; Well-typed if:
 ;; A. higher-order and ∎ and monitored
 ;; B. well-tagged under the given modality, and subterms well-typed
 ;; C. not well-tagged, and ◊ modality
+;; (This is basically 3 judgments in one,
+;;  first recur on the mode,
+;;  second recur on the value/type)
 (define-judgment-form MMT
-  #:mode (well-typed-value I I)
-  #:contract (well-typed-value v τ)
+  #:mode (well-typed-value I I I)
+  #:contract (well-typed-value σ v τ)
   [
    (not-well-tagged-value v k)
    ---
-   (well-typed-value v (k ◊ τ_k ...))]
+   (well-typed-value σ v (k ◊ τ_k ...))]
   [
-   (well-typed-value v (k □ τ_k ...))
+   (well-typed-value σ v (k □ τ_k ...))
    ---
-   (well-typed-value v (k ◊ τ_k ...))]
+   (well-typed-value σ v (k ◊ τ_k ...))]
   [
    (well-tagged-value v k)
-   (where (v_k ...) #{sub-values v})
-   (well-typed-value* (v_k ...) (τ_k ...))
+   (where (v_k ...) #{sub-values σ v}) ;; maybe fewer sub-values than τ
+   (well-typed-value* σ (v_k ...) (τ_k ...))
    ---
-   (well-typed-value v (k □ τ_k ...))]
+   (well-typed-value σ v (k □ τ_k ...))]
   [
    ---
-   (well-typed-value integer (Int ∎))]
+   (well-typed-value σ integer (Int ∎))]
   [
    ---
-   (well-typed-value boolean (Bool ∎))]
+   (well-typed-value σ boolean (Bool ∎))]
   [
-   (well-typed-value v_0 τ_0)
-   (well-typed-value v_1 τ_1)
+   (well-typed-value σ v_0 τ_0)
+   (well-typed-value σ v_1 τ_1)
    ---
-   (well-typed-value (cons v_0 v_1) (Pair ∎ τ_0 τ_1))]
+   (well-typed-value σ (cons v_0 v_1) (Pair ∎ τ_0 τ_1))]
   [
    (where (mon (→ ∎ τ_0 τ_1) v_arr) v)
    ---
-   (well-typed-value v (→ ∎ τ_0 τ_1))]
+   (well-typed-value σ v (→ ∎ τ_0 τ_1))]
   [
-   (where (mon τ v_box) v)
+   (where (mon (Box ∎ τ_box) (box x_box)) v)
+   (where v_box #{runtime-env-ref σ x_box})
+   (well-typed-value σ v_box τ_box)
    ---
-   (well-typed-value v (Box ∎ τ))])
+   (well-typed-value σ v (Box ∎ τ_box))])
 
 (define-judgment-form MMT
-  #:mode (well-typed-value* I I)
-  #:contract (well-typed-value* (v ...) (τ ...))
+  #:mode (well-typed-value* I I I)
+  #:contract (well-typed-value* σ (v ...) (τ ...))
   [
    --- Null
-   (well-typed-value* () (τ ...))]
+   (well-typed-value* σ () (τ ...))]
   [
-   (well-typed-value v_0 τ_0)
-   (well-typed-value* (v_1 ...) (τ_1 ...))
+   (well-typed-value σ v_0 τ_0)
+   (well-typed-value* σ (v_1 ...) (τ_1 ...))
    ---
-   (well-typed-value* (v_0 v_1 ...) (τ_0 τ_1 ...))])
+   (well-typed-value* σ (v_0 v_1 ...) (τ_0 τ_1 ...))])
 
 (define-judgment-form MMT
   #:mode (well-tagged-value I I)
@@ -155,7 +172,7 @@
    (well-tagged-value (cons v_0 v_1) Pair)]
   [
    ---
-   (well-tagged-value (fun f τ e) →)]
+   (well-tagged-value (fun _ _ _ _) →)]
   [
    ---
    (well-tagged-value (box _) Box)]
@@ -173,7 +190,83 @@
    (not-well-tagged-value v k)])
 
 (module+ test
-  ;; TODO
+  (define Int->Int (term (→ □ (Int □) (Int □))))
+
+  (test-case "well-tagged"
+    ;; box tests are a little weird,
+    ;;  because the boxes contain unbound variables,
+    ;;  because (box v) is not a value --- only (box x) is
+
+    (check-judgment-holds*
+     (well-tagged-value 3 Int)
+     (well-tagged-value #true Bool)
+     (well-tagged-value (cons 1 #false) Pair)
+     (well-tagged-value (fun f ,Int->Int (x) x) →)
+     (well-tagged-value (mon ,Int->Int (fun g ,Int->Int (x) 3))
+                        →)
+     (well-tagged-value (box x) Box)
+     (well-tagged-value (mon (Box □ (Int □)) (box z)) Box)
+    )
+    (check-not-judgment-holds*
+     (well-tagged-value 3 Bool)
+     (well-tagged-value #false →)
+     (well-tagged-value (mon (Box □ (Int □)) 4) Box)
+    )
+  )
+
+  (test-case "not-well-tagged"
+    (check-judgment-holds*
+     (not-well-tagged-value 3 Bool)
+     (not-well-tagged-value #false →)
+     (not-well-tagged-value (mon (Box □ (Int □)) 4) Box)
+    )
+    (check-not-judgment-holds*
+     (not-well-tagged-value 3 Int)
+     (not-well-tagged-value #true Bool)
+     (not-well-tagged-value (box a) Box)
+     (not-well-tagged-value (mon (Box □ (Int □)) (box b)) Box)
+    )
+  )
+
+  (test-case "well-typed-value"
+    (check-judgment-holds*
+     (well-typed-value () 4 (Int □))
+     (well-typed-value () #true (Bool ∎))
+     (well-typed-value () #true (Bool ◊))
+     (well-typed-value () #true (Int ◊))
+
+     (well-typed-value () (cons 1 1) (Pair □ (Int □) (Int □)))
+     (well-typed-value () (cons 1 1) (Pair ∎ (Int ∎) (Int ∎)))
+     (well-typed-value () (cons 1 1) (Pair ◊ (Int ◊) (Int ◊)))
+     (well-typed-value () (cons #true #true) (Pair ◊ (Int ◊) (Int ◊)))
+     (well-typed-value () 8888 (Pair ◊ (Int ◊) (Int ◊)))
+     (well-typed-value () (cons 1 #false) (Pair ◊ (Int □) (Int ◊)))
+     (well-typed-value () (cons 1 #false) (Pair □ (Int □) (→ ◊ (Int □) (Int □))))
+
+     (well-typed-value ((x 3)) (box x) (Box □ (Int □)))
+     (well-typed-value ((x #true)) (box x) (Box □ (Bool ◊)))
+     (well-typed-value ((x (fun f ,Int->Int (x) (+ x x)))) (box x) (Box □ ,Int->Int))
+     (well-typed-value ((z (fun f ,Int->Int (x) (+ x x)))) (box z) (Box □ (Int ◊)))
+     (well-typed-value ((z (fun f ,Int->Int (x) (+ x x)))) (box z) (Bool ◊))
+     (well-typed-value ((z 4)) (mon (Box ∎ (Int ∎)) (box z)) (Box ∎ (Int ∎)))
+
+     (well-typed-value () (fun f ,Int->Int (x) 4) (→ □ (Int □) (Int □)))
+     (well-typed-value () (fun f ,Int->Int (x) 4) (→ □ (Bool □) (Bool □)))
+     (well-typed-value () (fun f ,Int->Int (x) 4) (→ ◊ (Bool □) (Int ∎)))
+     (well-typed-value () (mon (→ ∎ (Int □) (Int ◊)) (fun f ,Int->Int (x) 4))
+                       (→ ∎ (Int □) (Int ◊)))
+    )
+
+    (check-not-judgment-holds*
+     ;; TODO simple tests
+
+     (well-typed-value ((z 4)) (box z) (Box ∎ (Int □)))
+     (well-typed-value ((z 4)) (box z) (Box ∎ (Int ◊)))
+     (well-typed-value ((z (fun f ,Int->Int (x) (+ x x)))) (box z) (Box ∎ ,Int->Int))
+     (well-typed-value ((z #true)) (mon (Box ∎ (Int ∎)) (box z)) (Box ∎ (Int ∎)))
+     (well-typed-value ((z 32)) (mon (Box ∎ (Int ∎)) (box z)) (Box ∎ (Bool ∎)))
+    )
+  )
 )
 
 ;; -----------------------------------------------------------------------------
@@ -241,19 +334,38 @@
    (μ<=? μ μ)])
 
 (define-metafunction MMT
-  sub-values : v -> (v ...)
-  [(sub-values integer)
+  sub-values : σ v -> (v ...)
+  [(sub-values σ integer)
    ()]
-  [(sub-values boolean)
+  [(sub-values σ boolean)
    ()]
-  [(sub-values (cons v_0 v_1))
+  [(sub-values σ (cons v_0 v_1))
    (v_0 v_1)]
-  [(sub-values Λ)
+  [(sub-values σ Λ)
    ()]
-  [(sub-values (box v_0))
-   (v_0)])
+  [(sub-values σ (box x_0))
+   (#{runtime-env-ref σ x_0})])
+
+(define-metafunction MMT
+  runtime-env-ref : σ x -> v
+  [(runtime-env-ref σ x)
+   ,(or (for/first ([xv (in-list (term σ))]
+                    #:when (equal? (car xv) (term x)))
+          (cadr xv))
+        (raise-arguments-error 'runtime-env-ref "unbound variable" "var" (term x) "env" (term σ)))])
 
 (module+ test
   ;; TODO
+
+  (test-case "runtime-env-ref"
+    (check-equal?
+      (term #{runtime-env-ref ((x 0) (y 1)) x})
+      0)
+    (check-equal?
+      (term #{runtime-env-ref ((x 0) (y 1)) y})
+      1)
+    (check-exn exn:fail:contract?
+      (λ () (term #{runtime-env-ref ((x 0) (y 1)) z})))
+  )
 )
 
