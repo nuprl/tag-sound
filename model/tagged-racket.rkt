303b7de74e8180a70a7cf07743a9ee096cf3fc57
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-29 23:14:01 -0400

    model: tagged evaluation

--- model/tagged-racket.rkt
+++ model/tagged-racket.rkt
@@ -62,10 +62,10 @@
          (let (x e) e)
          (:: Λ τ) ;; not allowed in untyped code
          (require (x τ e) e) ;; not allowed in untyped code
-         (unop e) (binop e e))
+         (unop e) (binop e e) (cons e e))
   (primop ::= unop binop)
   (unop ::= car cdr make-box unbox)
-  (binop ::= + * - = set-box! cons)
+  (binop ::= + * - = set-box!)
 
 ;; t = explicitly typed intermediate language, still allows untyped (source) terms
   (t ::= (:: v τ) (:: x τ) (:: (t t) τ) (:: (if t t t) τ)
@@ -73,12 +73,13 @@
          (:: (let (x t) t) τ)
          (:: (require (x τ e) t) τ)
          (:: (unop t) τ)
-         (:: (binop t t) τ))
+         (:: (binop t t) τ)
+         (:: (cons t t) τ))
 
 ;; c = type-erased, explicitly checked core language
-  (c ::= v x (c c) (if c c c) (let (x c) c) (unop c) (binop c c) (check K c))
+  (c ::= v x (c c) (if c c c) (let (x c) c) (unop c) (binop c c) (cons c c) (check K c))
   (σ ::= ((x v) ...))
-  (E ::= hole (E c) (v E) (if E c c) (let (x E) c) (unop E) (binop E c) (binop v E))
+  (E ::= hole (E c) (v E) (if E c c) (let (x E) c) (unop E) (binop E c) (binop v E) (cons E c) (cons v E) (check K E))
   (RuntimeError ::= (CheckError K v))
   (A ::= [σ e] RuntimeError)
 
@@ -134,7 +135,7 @@
      [(type-check# (+ 2 2))
       (:: (+ (:: 2 Int) (:: 2 Int)) Int)]
      [(type-check# (:: (fun f (x) #true) (→ Int Bool)))
-      (:: (fun f (x) (:: #false Bool)) (→ Int Bool))])
+      (:: (fun f (x) (:: #true Bool)) (→ Int Bool))])
     (void)))
 
 ;; -----------------------------------------------------------------------------
@@ -168,7 +169,7 @@
   [(eval# c)
    A_1
    (where A_0 #{load# c})
-   (where A_1 ,(eval* (term c)))]
+   (where A_1 ,(eval* (term A_0)))]
   [(eval# c)
    ,(raise-argument-error 'eval# "core term" (term c))])
 
@@ -177,12 +178,22 @@
   [(load# c)
    [() c]])
 
+(define-metafunction TAG
+  unload# : A -> any
+  [(unload# RuntimeError)
+   RuntimeError]
+  [(unload# [σ (box x)])
+   (box v)
+   (where v #{runtime-env-ref σ x})]
+  [(unload# [σ v])
+   v])
+
 (module+ test
   (test-case "eval:basic"
     (check-mf-apply*
-     [(eval# (+ 2 2))
+     [(unload# (eval# (+ 2 2)))
       4]
-     [(eval# ((fun fact (n) (if (= n 1) 1 (* n (fact (- n 1))))) 5))
+     [(unload# (eval# ((fun fact (n) (if (= n 1) 1 (* n (fact (- n 1))))) 5)))
       120])
     (void)))
 
@@ -236,6 +247,13 @@
    (where τ #{type-env-ref Γ x})
    ---
    (type-check Γ x (:: x τ))]
+  [
+   (type-check Γ e_0 t_0)
+   (type-check Γ e_1 t_1)
+   (where τ_0 #{type-annotation t_0})
+   (where τ_1 #{type-annotation t_1})
+   ---
+   (type-check Γ (cons e_0 e_1) (:: (cons t_0 t_1) (Pair τ_0 τ_1)))]
   [
    (type-check Γ e_0 t_0)
    (type-check Γ e_1 t_1)
@@ -299,71 +317,6 @@
   [(erase-types# any)
    any])
 
-(module+ test
-  (check α=?
-    (term (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int))))
-    (term (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int)))))
-
-  (test-case "type-check"
-    (check-mf-apply* #:is-equal? α=?
-     [(type-check# 4)
-      (:: 4 Int)]
-     [(type-check# #false)
-      (:: #false Bool)]
-     [(type-check# #true)
-      (:: #true Bool)]
-     [(type-check# (:: (fun f (x) x) (→ (Box Int) (Box Int))))
-      (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int)))]
-     ;[(type-check# (:: (fun f (x) x) (→ (Box Bool) (Box Bool))))
-     ; (:: (fun f (x) (:: x (Box Bool))) (→ (Box Bool) (Box Bool)))]
-     [(type-check# (cons 1 1))
-      (:: (cons (:: 1 Int) (:: 1 Int)) (Pair Int Int))]
-     [(type-check# ((:: (fun f (x) 4) (→ Int Int)) 3))
-      (:: ((:: (fun f (x) (:: 4 Int)) (→ Int Int)) (:: 3 Int)) Int)]
-     [(type-check# (if 1 2 3))
-      (:: (if (:: 1 Int) (:: 2 Int) (:: 3 Int)) Int)]
-     [(type-check# (let (x 4) x))
-      (:: (let (x (:: 4 Int)) (:: x Int)) Int)]
-     [(type-check# (require (x Int 1) x))
-      (:: (require (x Int 1) (:: x Int)) Int)]
-     [(type-check# (car (cons 1 2)))
-      (:: (car (:: (cons (:: 1 Int) (:: 2 Int)) (Pair Int Int))) Int)]
-     [(type-check# (cdr (cons 2 1)))
-      (:: (cdr (:: (cons (:: 2 Int) (:: 1 Int)) (Pair Int Int))) Int)]
-     [(type-check# (make-box 3))
-      (:: (make-box (:: 3 Int)) (Box Int))]
-     [(type-check# (unbox (make-box 3)))
-      (:: (unbox (:: (make-box (:: 3 Int)) (Box Int))) Int)]
-     [(type-check# (+ 1 1))
-      (:: (+ (:: 1 Int) (:: 1 Int)) Int)]
-     [(type-check# (- 1 1))
-      (:: (- (:: 1 Int) (:: 1 Int)) Int)]
-     [(type-check# (* 1 1))
-      (:: (* (:: 1 Int) (:: 1 Int)) Int)]
-     [(type-check# (= 1 1))
-      (:: (= (:: 1 Int) (:: 1 Int)) Bool)]
-     [(type-check# (set-box! (make-box 2) 3))
-      (:: (set-box! (:: (make-box (:: 2 Int)) (Box Int)) (:: 3 Int)) Int)])
-    (void))
-
-  (test-case "sound-elaboration"
-    (check-judgment-holds*
-     (sound-elaboration 4 (:: 4 Int))
-     (sound-elaboration (:: (fun f (x) x) (→ Int Int))
-                        (:: (fun f (x) (:: x Int)) (→ Int Int)))
-     (sound-elaboration (+ 2 2) (:: (+ (:: 2 Int) (:: 2 Int)) Int))
-     (sound-elaboration (make-box 3) (:: (make-box (:: 3 Int)) (Box Int))))
-    (void))
-
-  (test-case "erase-types"
-    (check-mf-apply* #:is-equal? α=?
-     [(erase-types# (:: 3 Int))
-      3]
-     [(erase-types# (:: (fun f (x) (:: 4 Int)) (→ Int Int)))
-      (fun f (x) 4)])
-    (void))
-)
-
 ;; =============================================================================
 ;; === completion
 
@@ -386,9 +339,39 @@
   (reduction-relation TAG
    #:domain A
    [-->
-    [σ c]
-    (CheckError Int 0)
-    (side-condition (raise-user-error '-->TAG "not implemented"))]))
+    [σ (in-hole E (Λ v))]
+    [σ (in-hole E c_v)]
+    E-Beta
+    (where (fun x_f (x) c) Λ)
+    (where c_f (substitute c x_f Λ))
+    (where c_v (substitute c_f x v))]
+   [-->
+    [σ (in-hole E (if v c_0 c_1))]
+    [σ (in-hole E c_next)]
+    E-If
+    (where c_next ,(if (term v) (term c_0) (term c_1)))]
+   [-->
+    [σ (in-hole E (let (x v) c))]
+    [σ (in-hole E c_x)]
+    E-Let
+    (where c_x (substitute c x v))]
+   [-->
+    [σ (in-hole E (primop v ...))]
+    #{maybe-in-hole E A}
+    E-PrimOp
+    (where A (apply-primop [σ (primop v ...)]))]
+   [-->
+    [σ (check K v)]
+    #{maybe-in-hole E A}
+    E-Check
+    (where A #{apply-check [σ (check K v)]})]))
+
+(define-metafunction TAG
+  maybe-in-hole : E A -> A
+  [(maybe-in-hole E RuntimeError)
+   Runtime-Error]
+  [(maybe-in-hole E [σ c])
+   [σ (in-hole E c)]])
 
 (define eval*
   (make--->* -->TAG))
@@ -737,8 +720,6 @@
    (→ Int (→ Int Bool))]
   [(primop-type set-box!)
    (→ (Box Int) (→ Int Int))]
-  [(primop-type cons)
-   (→ Int (→ Int (Pair Int Int)))]
   [(primop-type car)
    (→ (Pair Int Int) Int)]
   [(primop-type cdr)
@@ -748,6 +729,174 @@
   [(primop-type unbox)
    (→ (Box Int) Int)])
 
+(define-metafunction TAG
+  apply-primop : [σ (primop v ...)] -> A
+  [(apply-primop [σ (car (cons v_0 _))])
+   [σ v_0]]
+  [(apply-primop [σ (cdr (cons _ v_1))])
+   [σ v_1]]
+  [(apply-primop [σ (make-box v)])
+   [σ_x (box x)]
+   (where x #{fresh-location σ})
+   (where σ_x #{runtime-env-set σ (x v)})]
+  [(apply-primop [σ (unbox (box x))])
+   [σ v]
+   (where v #{runtime-env-ref σ x})]
+  [(apply-primop [σ (+ integer_0 integer_1)])
+   [σ v]
+   (where v ,(+ (term integer_0) (term integer_1)))]
+  [(apply-primop [σ (- integer_0 integer_1)])
+   [σ v]
+   (where v ,(- (term integer_0) (term integer_1)))]
+  [(apply-primop [σ (* integer_0 integer_1)])
+   [σ v]
+   (where v ,(* (term integer_0) (term integer_1)))]
+  [(apply-primop [σ (= integer_0 integer_1)])
+   [σ v]
+   (where v ,(= (term integer_0) (term integer_1)))]
+  [(apply-primop [σ (set-box! (box x) v)])
+   [σ_x v]
+   (where σ_x #{runtime-env-update σ (x v)})])
+
+(define-metafunction TAG
+  apply-check : [σ (check K v)] -> A
+  [(apply-check [σ (check K v)])
+   A_next
+   (where A_next [σ v])
+   (judgment-holds (value-check A_next K))]
+  [(apply-check [σ (check K v)])
+   (CheckError K v)])
+
+(define-metafunction TAG
+  runtime-env-set : σ (x v) -> σ
+  [(runtime-env-set σ (x v))
+   ,(cons (term (x v)) (term σ))])
+
+(define-metafunction TAG
+  runtime-env-ref : σ x -> v
+  [(runtime-env-ref σ x)
+   ,(or
+      (for/first ([xv (in-list (term σ))]
+                  #:when (equal? (car xv) (term x)))
+        (cadr xv))
+      (raise-arguments-error 'runtime-env-ref "unbound variable" "var" (term x) "env" (term σ)))])
+
+(define-metafunction TAG
+  runtime-env-update : σ (x v) -> σ
+  [(runtime-env-update σ (x v))
+   ,(let* ([success? (box #f)]
+           [σ+ (for/list ([xv (in-list (term σ))])
+                 (if (equal? (term x) (car xv))
+                   (begin (set-box! success? #true) (term (x v)))
+                   xv))])
+      (if (unbox success?)
+        σ+
+        (raise-arguments-error 'runtime-env-update "unbound variable" "var" (term x) "env" (term σ))))])
+
+(define-metafunction TAG
+  fresh-location : σ -> x
+  [(fresh-location σ)
+   any_0
+   (where any_0 ,(variable-not-in (term σ) 'loc))])
+
 ;; =============================================================================
 ;; === test
+
+(module+ test
+  (check α=?
+    (term (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int))))
+    (term (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int)))))
+
+  (test-case "type-check"
+    (check-mf-apply* #:is-equal? α=?
+     [(type-check# 4)
+      (:: 4 Int)]
+     [(type-check# #false)
+      (:: #false Bool)]
+     [(type-check# #true)
+      (:: #true Bool)]
+     [(type-check# (:: (fun f (x) x) (→ (Box Int) (Box Int))))
+      (:: (fun f (x) (:: x (Box Int))) (→ (Box Int) (Box Int)))]
+     ;[(type-check# (:: (fun f (x) x) (→ (Box Bool) (Box Bool))))
+     ; (:: (fun f (x) (:: x (Box Bool))) (→ (Box Bool) (Box Bool)))]
+     [(type-check# (cons 1 1))
+      (:: (cons (:: 1 Int) (:: 1 Int)) (Pair Int Int))]
+     [(type-check# ((:: (fun f (x) 4) (→ Int Int)) 3))
+      (:: ((:: (fun f (x) (:: 4 Int)) (→ Int Int)) (:: 3 Int)) Int)]
+     [(type-check# (if 1 2 3))
+      (:: (if (:: 1 Int) (:: 2 Int) (:: 3 Int)) Int)]
+     [(type-check# (let (x 4) x))
+      (:: (let (x (:: 4 Int)) (:: x Int)) Int)]
+     [(type-check# (require (x Int 1) x))
+      (:: (require (x Int 1) (:: x Int)) Int)]
+     [(type-check# (car (cons 1 2)))
+      (:: (car (:: (cons (:: 1 Int) (:: 2 Int)) (Pair Int Int))) Int)]
+     [(type-check# (cdr (cons 2 1)))
+      (:: (cdr (:: (cons (:: 2 Int) (:: 1 Int)) (Pair Int Int))) Int)]
+     [(type-check# (make-box 3))
+      (:: (make-box (:: 3 Int)) (Box Int))]
+     [(type-check# (unbox (make-box 3)))
+      (:: (unbox (:: (make-box (:: 3 Int)) (Box Int))) Int)]
+     [(type-check# (+ 1 1))
+      (:: (+ (:: 1 Int) (:: 1 Int)) Int)]
+     [(type-check# (- 1 1))
+      (:: (- (:: 1 Int) (:: 1 Int)) Int)]
+     [(type-check# (* 1 1))
+      (:: (* (:: 1 Int) (:: 1 Int)) Int)]
+     [(type-check# (= 1 1))
+      (:: (= (:: 1 Int) (:: 1 Int)) Bool)]
+     [(type-check# (set-box! (make-box 2) 3))
+      (:: (set-box! (:: (make-box (:: 2 Int)) (Box Int)) (:: 3 Int)) Int)])
+    (void))
+
+  (test-case "sound-elaboration"
+    (check-judgment-holds*
+     (sound-elaboration 4 (:: 4 Int))
+     (sound-elaboration (:: (fun f (x) x) (→ Int Int))
+                        (:: (fun f (x) (:: x Int)) (→ Int Int)))
+     (sound-elaboration (+ 2 2) (:: (+ (:: 2 Int) (:: 2 Int)) Int))
+     (sound-elaboration (make-box 3) (:: (make-box (:: 3 Int)) (Box Int))))
+    (void))
+
+  (test-case "erase-types"
+    (check-mf-apply* #:is-equal? α=?
+     [(erase-types# (:: 3 Int))
+      3]
+     [(erase-types# (:: (fun f (x) (:: 4 Int)) (→ Int Int)))
+      (fun f (x) 4)])
+    (void))
+)
+
+(module+ test
+  (test-case "eval:basic"
+    (check-mf-apply*
+     [(unload# (eval# 2))
+      2]
+     [(unload# (eval# ((fun f (x) x) 3)))
+      3]
+     [(unload# (eval# (if 1 2 3)))
+      2]
+     [(unload# (eval# (if #false 2 3)))
+      3]
+     [(unload# (eval# (let (x 2) x)))
+      2]
+     [(unload# (eval# (car (cons 1 2))))
+      1]
+     [(unload# (eval# (cdr (cons 1 2))))
+      2]
+     [(unload# (eval# (make-box 1)))
+      (box 1)]
+     [(unload# (eval# (unbox (make-box 1))))
+      1]
+     [(unload# (eval# (+ 2 2)))
+      4]
+     [(unload# (eval# (- 3 1)))
+      2]
+     [(unload# (eval# (* 4 4)))
+      16]
+     [(unload# (eval# (= 2 2)))
+      #true]
+     [(unload# (eval# (set-box! (make-box 3) 4)))
+      4])
+  (void)))
 
