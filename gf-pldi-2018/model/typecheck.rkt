025e1eaec4aa7c71a5a96fe53c309013a7a73a9f
Ben Greenman <benjaminlgreenman@gmail.com>
2017-10-11 13:59:44 -0400

    model: basic typecheck tests

--- gf-pldi-2018/model/typecheck.rkt
+++ gf-pldi-2018/model/typecheck.rkt
@@ -1,6 +1,13 @@
 #lang mf-apply racket/base
 
 (provide
+  <:
+  subtype?
+  not-well-tagged-value
+  well-tagged-value
+  flat-value
+  vector-value
+  fun-value
 )
 
 (require
@@ -9,9 +16,7 @@
   racket/set
   redex/reduction-semantics)
 
-;; -----------------------------------------------------------------------------
-;; --- typing
-;; -----------------------------------------------------------------------------
+;; =============================================================================
 
 (define-judgment-form μTR
   #:mode (<: I I)
@@ -23,7 +28,7 @@
 (define-metafunction μTR
   subtype? : τ τ -> boolean
   [(subtype? τ_0 τ_1)
-   (judgment-holds (<: τ_0 τ_1))])
+   ,(judgment-holds (<: τ_0 τ_1))])
 
 (define-judgment-form μTR
   #:mode (well-tagged-value I I)
@@ -93,37 +98,68 @@
    ---
    (fun-value (mon-fun τ v))])
 
-(define-judgment-form μTR
-  #:mode (tag-of I I)
-  #:contract (tag-of τ κ)
-  [
-   ---
-   (tag-of κ κ)]
-  [
-   ---
-   (tag-of (→ τ_0 τ_1) →)]
-  [
-   ---
-   (tag-of (Vectorof τ) Vector)]
-  [
-   ---
-   (tag-of (Listof τ) List)]
-  [
-   (tag-of τ κ) ...
-   ---
-   (tag-of (U τ ...) (U κ ...))]
-  [
-   (tag-of τ κ)
-   ---
-   (tag-of (∀ (α) τ) κ)]
-  [
-   (tag-of τ κ)
-   ---
-   (tag-of (μ (α) τ) κ)])
+;; =============================================================================
 
-(define-metafunction μTR
-  type->tag : τ -> κ
-  [(type->tag τ)
-   κ
-   (judgment-holds (tag-of τ κ))])
+(module+ test
+  (require rackunit redex-abbrevs)
 
+  (test-case "subtype?"
+    (check-mf-apply*
+     [(subtype? Int Int)
+      #true])
+  )
+
+  (test-case "well-tagged"
+    (check-judgment-holds*
+     (well-tagged-value 1 Nat)
+     (well-tagged-value 1 Int)
+     (well-tagged-value 1 (U Int Vector))
+     (well-tagged-value (vector 1 2 3) Vector)
+     (well-tagged-value (fun f (x) x) →)
+     (well-tagged-value (cons 1 (cons 3 nil)) List))
+
+    (check-not-judgment-holds*
+     (well-tagged-value 1 Vector)
+     (well-tagged-value (fun f (x) x) List)
+     (well-tagged-value (vector 8 8) List))
+
+    (check-judgment-holds*
+     (not-well-tagged-value 1 Vector))
+
+    (check-not-judgment-holds*
+     (not-well-tagged-value (fun f (x) x) →))
+  )
+
+  (test-case "flat-value"
+    (check-judgment-holds*
+     (flat-value 3)
+     (flat-value -2)
+     (flat-value nil))
+
+    (check-not-judgment-holds*
+     (flat-value (cons 3 nil))
+     (flat-value (fun f (x) x))
+     (flat-value (vector 3))))
+
+  (test-case "vector-value"
+    (check-judgment-holds*
+     (vector-value (vector 0 0))
+     (vector-value (vector))
+     (vector-value (mon-vector (Vectorof Int) (vector)))
+     (vector-value (mon-vector (Vectorof Nat) (vector nil))))
+
+    (check-not-judgment-holds*
+     (vector-value (fun f (x) 3))
+     (vector-value 4)
+     (vector-value (cons 3 nil))))
+
+  (test-case "fun-value"
+    (check-judgment-holds*
+     (fun-value (fun f (x) x))
+     (fun-value (mon-fun (→ Int Int) (fun f (x) (cons 0 nil)))))
+
+    (check-not-judgment-holds*
+     (fun-value -2)
+     (fun-value 2)
+     (fun-value (vector 0 0))))
+)
