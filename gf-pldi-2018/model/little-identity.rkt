7d145a20fb06308067e983a6bd84af5afe3a9c81
Ben Greenman <benjaminlgreenman@gmail.com>
2017-11-02 16:13:52 -0400

    model: identity, fix bugs reorganize

--- gf-pldi-2018/model/little-identity.rkt
+++ gf-pldi-2018/model/little-identity.rkt
@@ -1,6 +1,8 @@
 #lang mf-apply racket/base
 
+
 (require
+  "little-mixed.rkt"
   (only-in redex-abbrevs
     make--->*)
   redex/reduction-semantics)
@@ -10,217 +12,13 @@
 
 ;; =============================================================================
 
-(define-language LM
-  (e ::= v x (e e) (× e e) (BINOP e e) (UNOP e) (static τ e) (dynamic τ e))
-  (BINOP ::= + /)
-  (UNOP ::= fst snd)
-  (v ::= integer (× v v) Λ)
-  (Λ ::= (λ (x : τ) e) (λ (x) e))
-  (τ ::= Int Nat (× τ τ) (→ τ τ))
-  (E ::= hole (E e) (v E) (× E e) (× v E) (BINOP E e) (BINOP v E) (UNOP E)
-         (static τ E) (dynamic τ E))
-  (Γ ::= (x:τ Γ) ())
-  (x:τ ::= x (x : τ))
-  (A ::= e TE BE)
-  (BE ::= (Boundary-Error e string))
-  (TE ::= (Type-Error e string))
-  (MAYBE-τ ::= #f τ)
-  (x ::= variable-not-otherwise-mentioned)
-  #:binding-forms
-  (λ (x) e #:refers-to x)
-  (λ (x : τ) e #:refers-to x))
+(define-extended-language LM-identity
+  LM
+  (E ::= .... (static τ E) (dynamic τ E)))
 
-(module+ test
-  (test-case "valid terms"
-    (check-true (redex-match? LM e (term (+ 1 -4))))
-    (check-true (redex-match? LM e (term (f x))))
-    (check-true (redex-match? LM e (term (0 1))))))
-
-(define-judgment-form LM
-  #:mode (well-typed I I I)
-  #:contract (well-typed Γ e τ)
-  [
-   (infer-type Γ e τ_infer)
-   (subtype τ_infer τ)
-   ---
-   (well-typed Γ e τ)])
-
-(define-judgment-form LM
-  #:mode (subtype I I)
-  #:contract (subtype τ τ)
-  [
-   --- S-Nat
-   (subtype Nat Int)]
-  [
-   (subtype τ_0 τ_2)
-   (subtype τ_1 τ_3)
-   --- S-Pair
-   (subtype (× τ_0 τ_1) (× τ_2 τ_3))]
-  [
-   (subtype τ_2 τ_0)
-   (subtype τ_1 τ_3)
-   --- S-Fun
-   (subtype (→ τ_0 τ_1) (→ τ_2 τ_3))]
-  [
-   --- S-Refl
-   (subtype τ τ)])
-
-(define-metafunction LM
-  type-join : τ τ -> MAYBE-τ
-  [(type-join τ_0 τ_1)
-   τ_1
-   (judgment-holds (subtype τ_0 τ_1))]
-  [(type-join τ_0 τ_1)
-   τ_0
-   (judgment-holds (subtype τ_1 τ_0))])
-
-(define-judgment-form LM
-  #:mode (infer-type I I O)
-  #:contract (infer-type Γ e τ)
-  [
-   (where #true (negative? integer))
-   --- I-Int
-   (infer-type Γ integer Int)]
-  [
-   --- I-Nat
-   (infer-type Γ natural Nat)]
-  [
-   (infer-type Γ e_0 τ_0)
-   (infer-type Γ e_1 τ_1)
-   --- I-Pair
-   (infer-type Γ (× e_0 e_1) (× τ_0 τ_1))]
-  [
-   (where Γ_x ((x : τ_0) Γ))
-   (infer-type Γ_x e τ_1)
-   --- I-Fun
-   (infer-type Γ (λ (x : τ_0) e) (→ τ_0 τ_1))]
-  [
-   (where τ (type-env-ref Γ x))
-   --- I-Var
-   (infer-type Γ x τ)]
-  [
-   (infer-type Γ e_0 (→ τ_dom τ_cod))
-   (infer-type Γ e_1 τ_1)
-   (subtype τ_1 τ_dom)
-   --- I-App
-   (infer-type Γ (e_0 e_1) τ_cod)]
-  [
-   (infer-type Γ e_0 τ_0)
-   (infer-type Γ e_1 τ_1)
-   (subtype τ_0 Int)
-   (subtype τ_1 Int)
-   (where τ (type-join τ_0 τ_1))
-   --- I-+
-   (infer-type Γ (+ e_0 e_1) τ)]
-  [
-   (infer-type Γ e_0 τ_0)
-   (infer-type Γ e_1 τ_1)
-   (subtype τ_0 Int)
-   (subtype τ_1 Int)
-   (where τ (type-join τ_0 τ_1))
-   --- I-/
-   (infer-type Γ (/ e_0 e_1) τ)]
-  [
-   (infer-type Γ e (× τ_0 τ_1))
-   --- I-Fst
-   (infer-type Γ (fst e) τ_0)]
-  [
-   (infer-type Γ e (× τ_0 τ_1))
-   --- I-Snd
-   (infer-type Γ (snd e) τ_1)]
-  [
-   (well-dyn Γ e)
-   --- I-Dynamic
-   (infer-type Γ (dynamic τ e) τ)])
-
-(define-metafunction LM
-  negative? : integer -> boolean
-  [(negative? natural)
-   #false]
-  [(negative? integer)
-   #true])
-
-(define-metafunction LM
-  type-env-ref : Γ x -> MAYBE-τ
-  [(type-env-ref () x)
-   #false]
-  [(type-env-ref ((x : τ) Γ) x)
-   τ]
-  [(type-env-ref (x:τ Γ) x)
-   (type-env-ref Γ x)])
-
-(define-judgment-form LM
-  #:mode (well-dyn I I)
-  #:contract (well-dyn Γ e)
-  [
-   --- D-Int
-   (well-dyn Γ integer)]
-  [
-   (well-dyn Γ e_0)
-   (well-dyn Γ e_1)
-   --- D-Pair
-   (well-dyn Γ (× e_0 e_1))]
-  [
-   (where Γ_x (x Γ))
-   (well-dyn Γ_x e)
-   --- D-Fun
-   (well-dyn Γ (λ (x) e))]
-  [
-   (where #true (type-env-contains Γ x))
-   --- D-Var
-   (well-dyn Γ x)]
-  [
-   (well-dyn Γ e_0)
-   (well-dyn Γ e_1)
-   --- D-App
-   (well-dyn Γ (e_0 e_1))]
-  [
-   (well-dyn Γ e_0)
-   (well-dyn Γ e_1)
-   --- D-Binop
-   (well-dyn Γ (BINOP e_0 e_1))]
-  [
-   (well-dyn Γ e)
-   --- D-Unop
-   (well-dyn Γ (UNOP e))]
-  [
-   (well-typed Γ e τ)
-   --- D-Static
-   (well-dyn Γ (static τ e))])
-
-(define-metafunction LM
-  type-env-contains : Γ x -> boolean
-  [(type-env-contains () x)
-   #false]
-  [(type-env-contains (x Γ) x)
-   #true]
-  [(type-env-contains (x:τ Γ) x)
-   (type-env-contains Γ x)])
-
-(module+ test
-  (test-case "well-typed"
-    (check-true
-      (judgment-holds (well-typed () (+ 2 (dynamic Int 2)) Int)))
-    (check-true
-      (judgment-holds (well-typed () (+ 2 (dynamic Int (static Nat 3))) Int)))
-    (check-true
-      (judgment-holds (well-typed () (+ 2 (dynamic Int (λ (x) x))) Int)))
-    (check-true
-      (judgment-holds (well-typed () ((dynamic (→ Int Int) (λ (x) (+ x 42))) 1) Int))))
-
-  (test-case "well-dyn"
-    (check-true
-      (judgment-holds (well-dyn () (+ 2 (static Int 2)))))
-    (check-true
-      (judgment-holds (well-dyn () (+ 2 (static Int (dynamic Int (λ (x) x)))))))
-    (check-true
-      (judgment-holds (well-dyn () ((static (→ Int Int) (λ (x : Int) (+ x 1))) 4))))
-    (check-false
-      (judgment-holds (well-dyn () (λ (h : Nat) 2))))))
-
-;; This `mixed-step` is just a `dyn-step` that passes through boundaries.
-(define mixed-step
-  (reduction-relation LM
+;; This `identity-step` is just a `dyn-step` that passes through boundaries.
+(define identity-step
+  (reduction-relation LM-identity
     #:domain A
     (--> (in-hole E (v_0 v_1))
          (in-hole E e_subst)
@@ -235,7 +33,7 @@
     (--> (in-hole E (v_0 v_1))
          (Type-Error v_0 "procedure?")
          E-App-2
-         (side-condition (not (redex-match? LM Λ (term v_0)))))
+         (side-condition (not (redex-match? LM-identity Λ (term v_0)))))
     (--> (in-hole E (+ v_0 v_1))
          (in-hole E integer_2)
          E-+-0
@@ -243,10 +41,10 @@
          (where integer_1 v_1)
          (where integer_2 ,(+ (term integer_0) (term integer_1))))
     (--> (in-hole E (+ v_0 v_1))
-         (Type-Error ,(if (redex-match? LM integer (term v_0)) (term v_1) (term v_0)) "integer")
+         (Type-Error ,(if (redex-match? LM-identity integer (term v_0)) (term v_1) (term v_0)) "integer")
          E-+-1
-         (side-condition (not (and (redex-match? LM integer (term v_0))
-                                   (redex-match? LM integer (term v_1))))))
+         (side-condition (not (and (redex-match? LM-identity integer (term v_0))
+                                   (redex-match? LM-identity integer (term v_1))))))
     (--> (in-hole E (/ v_0 v_1))
          (in-hole E integer_2)
          E-/-0
@@ -261,10 +59,10 @@
          (where integer_1 v_1)
          (side-condition (zero? (term integer_1))))
     (--> (in-hole E (/ v_0 v_1))
-         (Type-Error ,(if (redex-match? LM integer (term v_0)) (term v_1) (term v_0)) "integer")
+         (Type-Error ,(if (redex-match? LM-identity integer (term v_0)) (term v_1) (term v_0)) "integer")
          E-/-2
-         (side-condition (not (and (redex-match? LM integer (term v_0))
-                                   (redex-match? LM integer (term v_1))))))
+         (side-condition (not (and (redex-match? LM-identity integer (term v_0))
+                                   (redex-match? LM-identity integer (term v_1))))))
     (--> (in-hole E (fst v))
          (in-hole E v_0)
          E-fst-0
@@ -272,7 +70,7 @@
     (--> (in-hole E (fst v))
          (Type-Error v "pair?")
          E-fst-1
-         (side-condition (not (redex-match? LM (× v_0 v_1) (term v)))))
+         (side-condition (not (redex-match? LM-identity (× v_0 v_1) (term v)))))
     (--> (in-hole E (snd v))
          (in-hole E v_1)
          E-snd-0
@@ -280,7 +78,7 @@
     (--> (in-hole E (snd v))
          (Type-Error v "pair?")
          E-snd-1
-         (side-condition (not (redex-match? LM (× v_0 v_1) (term v)))))
+         (side-condition (not (redex-match? LM-identity (× v_0 v_1) (term v)))))
     (--> (in-hole E (static τ v))
          (in-hole E v)
          E-static)
@@ -288,67 +86,70 @@
          (in-hole E v)
          E-dynamic)))
 
-(define mixed-step*
-  (make--->* mixed-step))
+(define identity-step*
+  (make--->* identity-step))
 
 (module+ test
-  (test-case "mixed-step*"
+  (test-case "identity-step*"
     (check-equal?
-      (mixed-step* (term (+ 2 2)))
+      (identity-step* (term (+ 2 2)))
       (term 4))
     (check-equal?
-      (mixed-step* (term ((λ (x) (+ x x)) 2)))
+      (identity-step* (term ((λ (x) (+ x x)) 2)))
       (term 4))
     (check-equal?
-      (mixed-step* (term ((λ (x : Int) (+ x x)) 2)))
+      (identity-step* (term ((λ (x : Int) (+ x x)) 2)))
       (term 4))
-    (check-true (redex-match? LM TE
-      (mixed-step* (term (0 0)))))
-    (check-true (redex-match? LM TE
-      (mixed-step* (term (+ (λ (z) z) 0)))))
-    (check-true (redex-match? LM TE
-      (mixed-step* (term (+ (λ (z : Int) z) 0)))))
-    (check-true (redex-match? LM BE
-      (mixed-step* (term (/ 1 0)))))
+    (check-true (redex-match? LM-identity TE
+      (identity-step* (term (0 0)))))
+    (check-true (redex-match? LM-identity TE
+      (identity-step* (term (+ (λ (z) z) 0)))))
+    (check-true (redex-match? LM-identity TE
+      (identity-step* (term (+ (λ (z : Int) z) 0)))))
+    (check-true (redex-match? LM-identity BE
+      (identity-step* (term (/ 1 0)))))
     (check-equal?
-      (mixed-step* (term (/ 1 3)))
+      (identity-step* (term (/ 1 3)))
       (term 0))
-    (check-true (redex-match? LM TE
-      (mixed-step* (term (fst 0)))))))
+    (check-true (redex-match? LM-identity TE
+      (identity-step* (term (fst 0)))))))
 
-(define-metafunction LM
+(define (assert-well-mixed t)
+  (unless (judgment-holds (well-mixed () ,t))
+    (raise-arguments-error 'safe-identity-step* "well-mixed expression" "term" t)))
+
+(define-metafunction LM-identity
   theorem:identity-safety : e MAYBE-τ -> any
   [(theorem:identity-safety e #f)
    ,(or (not (judgment-holds (well-dyn () e)))
-        (term #{safe-mixed-step* e}))]
+        (term #{safe-identity-step* e}))]
   [(theorem:identity-safety e τ)
    ,(or (not (judgment-holds (well-typed () e τ)))
-        (term #{safe-mixed-step* e}))])
+        (term #{safe-identity-step* e}))])
 
-(define-metafunction LM
-  safe-mixed-step* : A -> A
-  [(safe-mixed-step* TE)
+(define-metafunction LM-identity
+  safe-identity-step* : A -> A
+  [(safe-identity-step* TE)
    TE]
-  [(safe-mixed-step* BE)
+  [(safe-identity-step* BE)
    BE]
-  [(safe-mixed-step* e)
-   ,(raise-arguments-error 'safe-mixed-step* "well-mixed expression" "term" (term e))
-   (side-condition (not (judgment-holds (well-mixed () e))))]
-  [(safe-mixed-step* e)
-   ,(let ([A* (apply-reduction-relation mixed-step (term e))])
-      (cond
-       [(null? A*)
-        (term e)]
-       [(null? (cdr A*))
-        (term #{safe-mixed-step* ,(car A*)})]
-       [else
-        (raise-arguments-error 'safe-mixed-step* "mixed-step is non-deterministic for expression"
-          "e" (term e)
-          "answers" A*)]))])
+  [(safe-identity-step* e)
+   ,(begin
+      (void (assert-well-mixed (term e)))
+      (let ([A* (apply-reduction-relation identity-step (term e))])
+        (cond
+         [(null? A*)
+          (term e)]
+         [(null? (cdr A*))
+          (term #{safe-identity-step* ,(car A*)})]
+         [else
+          (raise-arguments-error 'safe-identity-step* "identity-step is non-deterministic for expression"
+            "e" (term e)
+            "answers" A*)])))])
 
 ;; `well-mixed` is invariant over evaluation,
 ;;  similar to `well-dyn` but allows more terms
-(define-judgment-form LM
+(define-judgment-form LM-identity
   #:mode (well-mixed I I)
   #:contract (well-mixed Γ e)
   [
@@ -360,17 +161,17 @@
    --- M-Pair
    (well-mixed Γ (× e_0 e_1))]
   [
-   (where Γ_x ,(cons (term x) (term Γ)))
+   (where Γ_x (x Γ))
    (well-mixed Γ_x e)
    --- M-Fun-0
    (well-mixed Γ (λ (x) e))]
   [
-   (where Γ_x ,(cons (term x) (term Γ)))
+   (where Γ_x (x Γ))
    (well-mixed Γ_x e)
    --- M-Fun-1
    (well-mixed Γ (λ (x : τ) e))]
   [
-   (where (x:τ_0 ... x x:τ_1 ...) Γ)
+   (where #true #{type-env-contains Γ x})
    --- M-Var
    (well-mixed Γ x)]
   [
@@ -397,15 +198,24 @@
    (well-mixed Γ (dynamic τ e))])
 
 (module+ test
+
+  (define (safe? t ty)
+    (and (term #{theorem:identity-safety ,t ,ty}) #true))
+
   (test-case "identity-safety"
+    (check-true (safe? (term ((× 0 2) (× 2 1))) #f))
+    (check-true (safe? (term (λ (n) (× 0 0))) #f))
+  )
+
+  (test-case "identity-safety:auto"
     (check-true
-      (redex-check LM #:satisfying (well-dyn () e)
-        (term (theorem:mixed-safety e #f))
-        #:attempts 1000
+      (redex-check LM-identity #:satisfying (well-dyn () e)
+        (term (theorem:identity-safety e #f))
+        #:attempts 10
         #:print? #f))
     (check-true
-      (redex-check LM #:satisfying (well-typed () e τ)
-        (term (theorem:mixed-safety e τ))
-        #:attempts 1000
+      (redex-check LM-identity #:satisfying (well-typed () e τ)
+        (term (theorem:identity-safety e τ))
+        #:attempts 10
         #:print? #f))))
 
---
+++ gf-pldi-2018/model/little-mixed.rkt
@@ -0,0 +1,226 @@
+#lang mf-apply racket/base
+
+(provide
+  LM
+  well-typed
+  well-dyn
+  type-env-contains
+)
+
+(require
+  (only-in redex-abbrevs
+    make--->*)
+  redex/reduction-semantics)
+
+(module+ test
+  (require rackunit))
+
+;; =============================================================================
+
+(define-language LM
+  (e ::= v x (e e) (× e e) (BINOP e e) (UNOP e) (static τ e) (dynamic τ e))
+  (BINOP ::= + /)
+  (UNOP ::= fst snd)
+  (v ::= integer (× v v) Λ)
+  (Λ ::= (λ (x : τ) e) (λ (x) e))
+  (τ ::= Int Nat (× τ τ) (→ τ τ))
+  (E ::= hole (E e) (v E) (× E e) (× v E) (BINOP E e) (BINOP v E) (UNOP E))
+  (Γ ::= (x:τ Γ) ())
+  (x:τ ::= x (x : τ))
+  (A ::= e TE BE)
+  (BE ::= (Boundary-Error e string))
+  (TE ::= (Type-Error e string))
+  (MAYBE-τ ::= #f τ)
+  (x ::= variable-not-otherwise-mentioned)
+  #:binding-forms
+  (λ (x) e #:refers-to x)
+  (λ (x : τ) e #:refers-to x))
+
+(module+ test
+  (test-case "valid terms"
+    (check-true (redex-match? LM e (term (+ 1 -4))))
+    (check-true (redex-match? LM e (term (f x))))
+    (check-true (redex-match? LM e (term (0 1))))))
+
+(define-judgment-form LM
+  #:mode (well-typed I I I)
+  #:contract (well-typed Γ e τ)
+  [
+   (infer-type Γ e τ_infer)
+   (subtype τ_infer τ)
+   ---
+   (well-typed Γ e τ)])
+
+(define-judgment-form LM
+  #:mode (subtype I I)
+  #:contract (subtype τ τ)
+  [
+   --- S-Nat
+   (subtype Nat Int)]
+  [
+   (subtype τ_0 τ_2)
+   (subtype τ_1 τ_3)
+   --- S-Pair
+   (subtype (× τ_0 τ_1) (× τ_2 τ_3))]
+  [
+   (subtype τ_2 τ_0)
+   (subtype τ_1 τ_3)
+   --- S-Fun
+   (subtype (→ τ_0 τ_1) (→ τ_2 τ_3))]
+  [
+   --- S-Refl
+   (subtype τ τ)])
+
+(define-metafunction LM
+  type-join : τ τ -> MAYBE-τ
+  [(type-join τ_0 τ_1)
+   τ_1
+   (judgment-holds (subtype τ_0 τ_1))]
+  [(type-join τ_0 τ_1)
+   τ_0
+   (judgment-holds (subtype τ_1 τ_0))])
+
+(define-judgment-form LM
+  #:mode (infer-type I I O)
+  #:contract (infer-type Γ e τ)
+  [
+   (where #true (negative? integer))
+   --- I-Int
+   (infer-type Γ integer Int)]
+  [
+   --- I-Nat
+   (infer-type Γ natural Nat)]
+  [
+   (infer-type Γ e_0 τ_0)
+   (infer-type Γ e_1 τ_1)
+   --- I-Pair
+   (infer-type Γ (× e_0 e_1) (× τ_0 τ_1))]
+  [
+   (where Γ_x ((x : τ_0) Γ))
+   (infer-type Γ_x e τ_1)
+   --- I-Fun
+   (infer-type Γ (λ (x : τ_0) e) (→ τ_0 τ_1))]
+  [
+   (where τ (type-env-ref Γ x))
+   --- I-Var
+   (infer-type Γ x τ)]
+  [
+   (infer-type Γ e_0 (→ τ_dom τ_cod))
+   (infer-type Γ e_1 τ_1)
+   (subtype τ_1 τ_dom)
+   --- I-App
+   (infer-type Γ (e_0 e_1) τ_cod)]
+  [
+   (infer-type Γ e_0 τ_0)
+   (infer-type Γ e_1 τ_1)
+   (subtype τ_0 Int)
+   (subtype τ_1 Int)
+   (where τ (type-join τ_0 τ_1))
+   --- I-+
+   (infer-type Γ (+ e_0 e_1) τ)]
+  [
+   (infer-type Γ e_0 τ_0)
+   (infer-type Γ e_1 τ_1)
+   (subtype τ_0 Int)
+   (subtype τ_1 Int)
+   (where τ (type-join τ_0 τ_1))
+   --- I-/
+   (infer-type Γ (/ e_0 e_1) τ)]
+  [
+   (infer-type Γ e (× τ_0 τ_1))
+   --- I-Fst
+   (infer-type Γ (fst e) τ_0)]
+  [
+   (infer-type Γ e (× τ_0 τ_1))
+   --- I-Snd
+   (infer-type Γ (snd e) τ_1)]
+  [
+   (well-dyn Γ e)
+   --- I-Dynamic
+   (infer-type Γ (dynamic τ e) τ)])
+
+(define-metafunction LM
+  negative? : integer -> boolean
+  [(negative? natural)
+   #false]
+  [(negative? integer)
+   #true])
+
+(define-metafunction LM
+  type-env-ref : Γ x -> MAYBE-τ
+  [(type-env-ref () x)
+   #false]
+  [(type-env-ref ((x : τ) Γ) x)
+   τ]
+  [(type-env-ref (x:τ Γ) x)
+   (type-env-ref Γ x)])
+
+(define-judgment-form LM
+  #:mode (well-dyn I I)
+  #:contract (well-dyn Γ e)
+  [
+   --- D-Int
+   (well-dyn Γ integer)]
+  [
+   (well-dyn Γ e_0)
+   (well-dyn Γ e_1)
+   --- D-Pair
+   (well-dyn Γ (× e_0 e_1))]
+  [
+   (where Γ_x (x Γ))
+   (well-dyn Γ_x e)
+   --- D-Fun
+   (well-dyn Γ (λ (x) e))]
+  [
+   (where #true (type-env-contains Γ x))
+   --- D-Var
+   (well-dyn Γ x)]
+  [
+   (well-dyn Γ e_0)
+   (well-dyn Γ e_1)
+   --- D-App
+   (well-dyn Γ (e_0 e_1))]
+  [
+   (well-dyn Γ e_0)
+   (well-dyn Γ e_1)
+   --- D-Binop
+   (well-dyn Γ (BINOP e_0 e_1))]
+  [
+   (well-dyn Γ e)
+   --- D-Unop
+   (well-dyn Γ (UNOP e))]
+  [
+   (well-typed Γ e τ)
+   --- D-Static
+   (well-dyn Γ (static τ e))])
+
+(define-metafunction LM
+  type-env-contains : Γ x -> boolean
+  [(type-env-contains () x)
+   #false]
+  [(type-env-contains (x Γ) x)
+   #true]
+  [(type-env-contains (x:τ Γ) x)
+   (type-env-contains Γ x)])
+
+(module+ test
+  (test-case "well-typed"
+    (check-true
+      (judgment-holds (well-typed () (+ 2 (dynamic Int 2)) Int)))
+    (check-true
+      (judgment-holds (well-typed () (+ 2 (dynamic Int (static Nat 3))) Int)))
+    (check-true
+      (judgment-holds (well-typed () (+ 2 (dynamic Int (λ (x) x))) Int)))
+    (check-true
+      (judgment-holds (well-typed () ((dynamic (→ Int Int) (λ (x) (+ x 42))) 1) Int))))
+
+  (test-case "well-dyn"
+    (check-true
+      (judgment-holds (well-dyn () (+ 2 (static Int 2)))))
+    (check-true
+      (judgment-holds (well-dyn () (+ 2 (static Int (dynamic Int (λ (x) x)))))))
+    (check-true
+      (judgment-holds (well-dyn () ((static (→ Int Int) (λ (x : Int) (+ x 1))) 4))))
+    (check-false
+      (judgment-holds (well-dyn () (λ (h : Nat) 2))))))
+
